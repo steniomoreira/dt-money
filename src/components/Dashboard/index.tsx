d6d9bdd5a194f4495b437d43b02eb6d52a88f63a
Stenio Moreira <steniomoreira01@gmail.com>
2021-09-03 01:35:43 -0300

    Adicionado TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 import { Container } from "./styles";
 
@@ -7,6 +8,7 @@ export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
